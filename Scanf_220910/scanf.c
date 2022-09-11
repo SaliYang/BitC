82cc379023a9b28eaf97740df2ad93987559792c
Yang <1530217467@qq.com>
2022-09-11 22:22:46 +0800

    初识常量变量

---
+++ Scanf_220910/scanf.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1//scanf函数在VS中报错，需使用此行代码
+#include <stdio.h>
+int main()//一个工程有且只能有一个主函数
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf ("%d %d", & a, & b);//函数命令对象在""内，储值在""外括号内
+	sum = a + b;
+	printf("sum=%d",sum);
+	return 0;
+}
