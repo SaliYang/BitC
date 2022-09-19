4051e3dcd040a7baab63e83eaffd19e16b1024cd
yang <1530217467@qq.com>
2022-09-19 17:32:01 +0800

    分支语句switch

---
+++ Switch_220919/Switch_220919/switch.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int day = 0;
+	scanf("%d", &day);
+
+	switch(day)
+	{
+		case(1):
+			printf("周一");
+			break;
+
+		case(2):
+			printf("周二");
+			break;
+
+		case(3):
+			printf("周三");
+			break;
+
+		case(4):
+			printf("周四");
+			break;
+
+		case(5):
+			printf("周五");
+			break;
+
+		case(6)://合理利用不加break实现特定功能
+		case(7):
+			printf("周末");
+			break;
+
+		default://switch末尾加上default是一个良好习惯
+			printf("无效值");
+	}
+
+	return 0;
+}
