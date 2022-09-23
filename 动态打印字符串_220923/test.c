7f794784872c0925c79fc49e86a6e3e95bd03d03
Yang <1530217467@qq.com>
2022-09-23 23:09:57 +0800

    循环语句练习

---
+++ 动态打印字符串_220923/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+
+int main()
+{
+	int left = 0;
+	int right = 0;
+
+	while(left == right)
+	{
+		//灵活利用初始化达到特定目的
+		char arr1[] = { "welcome to Bit!!!" };
+		char arr2[] = { "#################" };
+		int left = 0;
+		int right = strlen(arr1) - 1;
+
+		while (left < right)
+		{
+			arr2[left] = arr1[left];
+			arr2[right] = arr1[right];
+
+			printf("%s\n", arr2);
+
+			Sleep(1000);//<windows.h>
+			system("cls");
+
+			left++;
+			right--;
+		}
+	}
+	return 0;
+}
