ade6283cd9cf18efa9d798bd931c200246661815
Yang <1530217467@qq.com>
2022-09-25 21:15:41 +0800

    ifelse和循环语句使用方法

---
+++ 02分支循环语句/test_阶乘_220920.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	int n = 3;
+//	int ret = 1;
+//	int sum = 0;
+//
+//
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//		sum += ret;
+//	}
+//
+//	printf("sum=%d", sum);
+//	return 0;
+//}
