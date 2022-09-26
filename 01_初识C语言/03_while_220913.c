abfb868a0000d1590c29d0c6bbd5ac70b3ee26c3
Yang <1530217467@qq.com>
2022-09-26 22:26:03 +0800

    增加序号整理排版

---
+++ 01_初识C语言/03_while_220913.c
@@ -0,0 +1,24 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//extern int add(int x, int y);
+//int sum_input = 0;
+//int main()
+//{
+//	while (sum_input != 10)//while和if括号后不加；
+//	{
+//		printf("请输入2位数\n");
+//		int num1 = 0;
+//		int num2 = 0;
+//		scanf("%d%d", &num1, &num2);//scanf输入（）内加入\n，则第一次计算结果不会被返回
+//		int sum_input = add(num1, num2);
+//		printf("sum_input=%d\n", sum_input);
+//		if (sum_input == 10)//注意==和=的区别
+//		{
+//			printf("回答正确\n");
+//			break;
+//		}
+//	}
+//
+//	return 0;
+//}
