7f3b80f22c3c04a1f0adef1eb086ce8ccad177fd
SaliYang <113241584+SaliYang@users.noreply.github.com>
2022-10-09 22:06:45 +0800

    Update 01_递归_221009.c

--- 03_函数/01_递归_221009.c
+++ 03_函数/01_递归_221009.c
@@ -1,14 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-//��ϰ������һ���޷�������ֵ����˳���ӡÿһλ
+//练习：接收一个无符号整型值，按顺序打印每一位
 
 #include <stdio.h>
-//���ݹ��ݹ�̫��ᵼ��StackOverFlow
-//http;//stackoverflow.comȫ�����Ա��֪��
+//死递归或递归太深会导致StackOverFlow
+//http://stackoverflow.com全球程序员的知乎
 void print(unsigned int num)
 {
-	if (num > 9)//����Ҫ�еݹ���������
+	if (num > 9)//必须要有递归限制条件
 	{
-		print(num / 10);//ÿ�εݹ���ø��ӽ���������
+		print(num / 10);//每次递归调用更接近限制条件
 	}
 
 	printf("%d ", num % 10);
